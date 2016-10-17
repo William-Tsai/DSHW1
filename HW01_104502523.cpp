e46a403cd8e113ec762cae09ea38c4de8fc4e1cc
William-Tsai <williamtsai1111@gmail.com>
2016-10-18 00:11:34 +0800

    Implemented fucntion `save_polynomials`.

--- HW01_104502523.cpp
+++ HW01_104502523.cpp
@@ -17,7 +17,50 @@ typedef struct polynomials {
 /* save the input polynomials           */
 void save_polynomials(char *polynomial,polynomials *p)
 {
+    char *head = polynomial;
+    for(char *i = polynomial; i == polynomial || *(i - 1) != '\0'; i++){
+	if(*i == '+' || *i == '-' || *i == '\0'){
+	    //printf("Saving one terms...\n");
+	    // parse the polynomials term by term
+	    int exp = 0;
+	    if(i != polynomial && *(i - 1) != '^'){
+		for(char *j = head; j <= i; j++){
+		    if(*j == 'x' || *j == '\0'){
+			if(j != head){
+			    if(j - head != 1){
+		                char tmp[100];
+				int n = (j - head) / sizeof(char) + 1;
+			        strncpy(tmp, head, n);
+				tmp[n] = '\0';
+			        p->terms[p->length].coef = atoi(tmp);
+			    }else{
+				if(*head == '+') p->terms[p->length].coef = 1;
+				else if(*head == '-') p->terms[p->length].coef = -1;
+				else p->terms[p->length].coef = *head - '0';
+			    }
 
+			}
+			else p->terms[p->length].coef = 1;
+			head = j + 1;
+			exp = *j == '\0' ? 0 : 1;
+			break;
+		    }
+		}
+		if(*head == '^'){
+		    char tmp[100];
+		    int n = (i - head) / sizeof(char);
+		    head++;
+		    strncpy(tmp, head, n);
+		    tmp[n] = '\0';
+		    p->terms[p->length].exp = atoi(tmp);
+		}else p->terms[p->length].exp = exp;
+		p->length++;
+		//printf("%d: %dx^%d\n", p->length, p->terms[p->length - 1].coef, p->terms[p->length - 1].exp);
+	    }
+	    head = i;
+	}
+	
+    }
 }
 /* do differential of input polynomials */
 void polynomials_differential(polynomials *p)
